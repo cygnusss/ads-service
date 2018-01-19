b5265ec3a25b6eda6f9a19b105c49c69745f0278
Ubuntu <ubuntu@ip-172-31-27-140.us-west-1.compute.internal>
2018-01-19 00:59:45 +0000

    sets up cassandra for ec2

--- database_redis/index.js
+++ database_redis/index.js
@@ -1,7 +1,7 @@
 import redis from 'redis'
 import Promise from 'bluebird'
 
-export const redisClient = redis.createClient()
+export const redisClient = redis.createClient('6379', '172.31.7.89', {no_ready_check: true})
 
 Promise.promisifyAll(redis.RedisClient.prototype)
 
